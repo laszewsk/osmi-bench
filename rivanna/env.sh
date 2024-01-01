9fc5d35460836ee5aa2285296e59d0d39b35b6d7
Gregor von Laszewski <laszewski@gmail.com>
2024-01-01 11:46:50 -0500

    improve env.sh and image

--- rivanna/env.sh
+++ rivanna/env.sh
@@ -18,14 +18,21 @@ echo "OSMI_PROJECT:        " $OSMI_PROJECT
 echo "OSMI_TARGET:         " $OSMI_TARGET
 echo "============================================================="
 
-echo "Load apptainer ..."
+echo -n "Load   apptainer ... "
 module load apptainer
+echo "done"
 
-echo "Create Python OSMI environment ..."
+
+echo -n "Load   python/3.11.4 ... " 
 module load gcc/11.4.0  openmpi/4.1.4 python/3.11.4
+echo "done"
+echo -n "Create $USER_SCRATCH/OSMI/bin/python ... "
 python -m venv $USER_SCRATCH/OSMI # takes about 5.2s
+echo "done"
+echo -n "Load   Python $USER_SCRATCH/OSMI/bin/activate ... "
 source $USER_SCRATCH/OSMI/bin/activate
 pip install --upgrade pip > /dev/null
+echo "done"
 
 echo "============================================================="
 echo "Python version: " $(python --version)
